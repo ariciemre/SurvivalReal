126f3b6dbe2c2e5687a7c2f53d89d370b3c43fc1
ariciemre <ariciemre1567@gmail.com>
2023-11-02 20:55:58 +0300

    Bolum 1

---
+++ Assets/Scenes/Scripts/MouseMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float mouseSensivity = 200f;
+
+    float xRotation = 0;
+    float YRotation = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        YRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, YRotation,0f); // döndürme 
+    }
+}
